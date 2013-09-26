998b32384ab33906962c1a917671ca3a39c572db
Uttam Sharma <upsharma@west.com>
2013-09-26 11:58:42 -0500

    initialized

---
+++ MvcApplication1/App_Start/BundleConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace MvcApplication1
+{
+    public static class BundleConfiguration
+    {
+        public static void RegisterBundles(BundleCollection bundles) {
+            var myBundle = new ScriptBundle("~/bundles/js");
+            myBundle.Include("~/Scripts/jquery-2.0.3.min.js");
+        }
+
+    }
+}
---
+++ MvcApplication1/Models/WAMQA.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcApplication1.Models
+{
+    public class Form
+    {
+        public int ItemHandle { get; set; }
+        public int FormHandle { get; set; }
+        public int itemType { get; set; }
+        public int ItemText { get; set; }
+    }
+}
